c68353815cb44ab3d4843aec60e58ea32e0201ff
xiankelai <xiankelai@ybm100.com>
2019-11-08 14:44:41 +0800

    过滤文件

---
+++ spring-boot-learn-web/src/main/resources/db/tag.sql
@@ -0,0 +1,3 @@
+CREATE database spring-boot-learn DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE DATABASE IF NOT EXISTS spring_boot_learn DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
